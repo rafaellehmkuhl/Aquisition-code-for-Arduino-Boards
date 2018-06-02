acc198eaee514382a46f3c379ff17a932834b32f
Unknown <rafael.lehmkuhl93@gmail.com>
2018-06-01 22:54:02 -0300

    Move PitotThread and CellsThread to respectives .h files

---
+++ pitots_cells_threads/CellsThread.h
@@ -0,0 +1,83 @@
+class CellsThread: public Thread
+{
+public:
+  float forca_horizontal;
+  float forca_frontal_direita;
+  float forca_frontal_esquerda;
+  float forca_traseira_direita;
+  float forca_traseira_esquerda;
+
+  HX711_ADC &Celula_Horizontal;
+  HX711_ADC &Celula_FrontalDireita;
+  HX711_ADC &Celula_FrontalEsquerda;
+  HX711_ADC &Celula_TraseiraDireita;
+  HX711_ADC &Celula_TraseiraEsquerda;
+
+  CellsThread(HX711_ADC &Celula_Horizontal,
+              HX711_ADC &Celula_FrontalDireita,
+              HX711_ADC &Celula_FrontalEsquerda,
+              HX711_ADC &Celula_TraseiraDireita,
+              HX711_ADC &Celula_TraseiraEsquerda){};
+
+  void initializeCells(){
+    Celula_Horizontal.begin();
+    Celula_FrontalDireita.begin();
+    Celula_FrontalEsquerda.begin();
+    Celula_TraseiraDireita.begin();
+    Celula_TraseiraEsquerda.begin();
+
+    long calibrationTime = 5000; // tare preciscion can be improved by adding a few seconds of stabilising time
+
+    byte Celula_Horizontal_ready = 0;
+    byte Celula_FrontalDireita_ready = 0;
+    byte Celula_FrontalEsquerda_ready = 0;
+    byte Celula_TraseiraDireita_ready = 0;
+    byte Celula_TraseiraEsquerda_ready = 0;
+
+    // Run startup, stabilization and tare, all modules simultaniously
+    while ((Celula_Horizontal_ready + Celula_FrontalDireita_ready + Celula_FrontalEsquerda_ready + Celula_TraseiraDireita_ready + Celula_TraseiraEsquerda_ready) < 5) {
+      if (!Celula_Horizontal_ready) Celula_Horizontal_ready = Celula_Horizontal.startMultiple(calibrationTime);
+      if (!Celula_FrontalDireita_ready) Celula_FrontalDireita_ready = Celula_FrontalDireita.startMultiple(calibrationTime);
+      if (!Celula_FrontalEsquerda_ready) Celula_FrontalEsquerda_ready = Celula_FrontalEsquerda.startMultiple(calibrationTime);
+      if (!Celula_TraseiraDireita_ready) Celula_TraseiraDireita_ready = Celula_TraseiraDireita.startMultiple(calibrationTime);
+      if (!Celula_TraseiraEsquerda_ready) Celula_TraseiraEsquerda_ready = Celula_TraseiraEsquerda.startMultiple(calibrationTime);
+    }
+
+    // Calibrate cells
+    Celula_Horizontal.setCalFactor(2581.0); // user set calibration factor (float)
+    Celula_FrontalDireita.setCalFactor(744.0); // user set calibration factor (float)
+    Celula_FrontalEsquerda.setCalFactor(744.0); // user set calibration factor (float)
+    Celula_TraseiraDireita.setCalFactor(744.0); // user set calibration factor (float)
+    Celula_TraseiraEsquerda.setCalFactor(744.0); // user set calibration factor (float)
+
+    Serial.println("Startup and tare are complete");
+  }
+
+  void tareCells(){
+
+    // tare cells
+    Celula_Horizontal.tareNoDelay();
+    Celula_FrontalDireita.tareNoDelay();
+    Celula_FrontalEsquerda.tareNoDelay();
+    Celula_TraseiraDireita.tareNoDelay();
+    Celula_TraseiraEsquerda.tareNoDelay();
+  }
+
+  void run(){
+    // Update data on cells
+    Celula_Horizontal.update();
+    Celula_FrontalDireita.update();
+    Celula_FrontalEsquerda.update();
+    Celula_TraseiraDireita.update();
+    Celula_TraseiraEsquerda.update();
+
+    // Get data from cells
+    forca_horizontal = Celula_Horizontal.getData();
+    forca_frontal_direita = Celula_FrontalDireita.getData();
+    forca_frontal_esquerda = Celula_FrontalEsquerda.getData();
+    forca_traseira_direita = Celula_TraseiraDireita.getData();
+    forca_traseira_esquerda = Celula_TraseiraEsquerda.getData();
+
+    runned();
+  }
+};
--- pitots_cells_threads/pitots_cells_threads.ino
+++ pitots_cells_threads/pitots_cells_threads.ino
@@ -6,6 +6,9 @@
 #include <Adafruit_ADS1015.h>
 #include <HX711_ADC.h>
 
+#include "PitotThread.h"
+#include "CellsThread.h"
+
 const byte numChars = 32;
 char receivedChars[numChars];
 boolean newData = false;
@@ -34,96 +37,6 @@ HX711_ADC Celula_FrontalEsquerda(7, 8);
 HX711_ADC Celula_TraseiraDireita(9, 10);
 HX711_ADC Celula_TraseiraEsquerda(11, 12);
 
-class PitotThread: public Thread
-{
-public:
-  float Voltage = 0.0;
-  int16_t adc = 0;
-  int adc_port;
-  String apelido;
-  Adafruit_ADS1015 &ads;
-  PitotThread(){};
-  PitotThread(int adc_port, String apelido, Adafruit_ADS1015 &ads){};
-
-  void run(){
-    adc = ads.readADC_SingleEnded(adc_port);
-    Voltage = (adc * 0.1875)/1000;
-    runned();
-  }
-};
-
-class CellsThread: public Thread
-{
-public:
-  float forca_horizontal;
-  float forca_frontal_direita;
-  float forca_frontal_esquerda;
-  float forca_traseira_direita;
-  float forca_traseira_esquerda;
-
-  void initializeCells(){
-    Celula_Horizontal.begin();
-    Celula_FrontalDireita.begin();
-    Celula_FrontalEsquerda.begin();
-    Celula_TraseiraDireita.begin();
-    Celula_TraseiraEsquerda.begin();
-
-    long calibrationTime = 5000; // tare preciscion can be improved by adding a few seconds of stabilising time
-
-    byte Celula_Horizontal_ready = 0;
-    byte Celula_FrontalDireita_ready = 0;
-    byte Celula_FrontalEsquerda_ready = 0;
-    byte Celula_TraseiraDireita_ready = 0;
-    byte Celula_TraseiraEsquerda_ready = 0;
-
-    // Run startup, stabilization and tare, all modules simultaniously
-    while ((Celula_Horizontal_ready + Celula_FrontalDireita_ready + Celula_FrontalEsquerda_ready + Celula_TraseiraDireita_ready + Celula_TraseiraEsquerda_ready) < 5) {
-      if (!Celula_Horizontal_ready) Celula_Horizontal_ready = Celula_Horizontal.startMultiple(calibrationTime);
-      if (!Celula_FrontalDireita_ready) Celula_FrontalDireita_ready = Celula_FrontalDireita.startMultiple(calibrationTime);
-      if (!Celula_FrontalEsquerda_ready) Celula_FrontalEsquerda_ready = Celula_FrontalEsquerda.startMultiple(calibrationTime);
-      if (!Celula_TraseiraDireita_ready) Celula_TraseiraDireita_ready = Celula_TraseiraDireita.startMultiple(calibrationTime);
-      if (!Celula_TraseiraEsquerda_ready) Celula_TraseiraEsquerda_ready = Celula_TraseiraEsquerda.startMultiple(calibrationTime);
-    }
-
-    // Calibrate cells
-    Celula_Horizontal.setCalFactor(2581.0); // user set calibration factor (float)
-    Celula_FrontalDireita.setCalFactor(744.0); // user set calibration factor (float)
-    Celula_FrontalEsquerda.setCalFactor(744.0); // user set calibration factor (float)
-    Celula_TraseiraDireita.setCalFactor(744.0); // user set calibration factor (float)
-    Celula_TraseiraEsquerda.setCalFactor(744.0); // user set calibration factor (float)
-
-    Serial.println("Startup and tare are complete");
-  }
-
-  void tareCells(){
-
-    // tare cells
-    Celula_Horizontal.tareNoDelay();
-    Celula_FrontalDireita.tareNoDelay();
-    Celula_FrontalEsquerda.tareNoDelay();
-    Celula_TraseiraDireita.tareNoDelay();
-    Celula_TraseiraEsquerda.tareNoDelay();
-  }
-
-  void run(){
-    // Update data on cells
-    Celula_Horizontal.update();
-    Celula_FrontalDireita.update();
-    Celula_FrontalEsquerda.update();
-    Celula_TraseiraDireita.update();
-    Celula_TraseiraEsquerda.update();
-
-    // Get data from cells
-    forca_horizontal = Celula_Horizontal.getData();
-    forca_frontal_direita = Celula_FrontalDireita.getData();
-    forca_frontal_esquerda = Celula_FrontalEsquerda.getData();
-    forca_traseira_direita = Celula_TraseiraDireita.getData();
-    forca_traseira_esquerda = Celula_TraseiraEsquerda.getData();
-
-    runned();
-  }
-};
-
 ThreadController controller = ThreadController();
 
 PitotThread pitots[] = {
@@ -145,7 +58,11 @@ PitotThread pitots[] = {
   PitotThread(3, "pitot15", ads3)
 };
 
-CellsThread celulas_bancada = CellsThread();
+CellsThread celulas_bancada = CellsThread(Celula_Horizontal,
+                                          Celula_FrontalDireita,
+                                          Celula_FrontalEsquerda,
+                                          Celula_TraseiraDireita,
+                                          Celula_TraseiraEsquerda);
 
 void setup(){
   Serial.begin(115200);
@@ -247,7 +164,7 @@ void sendDataViaProtocol(){
 }
 
 void printProtocolled(String apelido, float value){
-  printf("%s=%f;", apelido, value);
+  printf("%s=%f;", apelido.c_str(), value);
 }
 
 void receiveCommands() {
