934d3ce4f6ce0594497fe789323af189431e0342
Unknown <rafael.lehmkuhl93@gmail.com>
2018-06-02 21:24:21 -0300

    Create Pitot and Cell classes
    Pitot and Cell classes are meant to unite the parameters, like Force/Voltage and Apelido.

---
+++ pitots_cells_threads/Pitot.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Pitot
+{
+    public:
+        float Voltage = 0.0;
+        int16_t adc = 0;
+        int adc_port;
+        String apelido;
+        Adafruit_ADS1015 &ads;
+        Pitot(int adc_port, String apelido, Adafruit_ADS1015 &ads){};
+
+    void updateVoltage(){
+        adc = ads.readADC_SingleEnded(adc_port);
+        Voltage = adc * 0.1875;
+    }
+};
